c90aae3757962f15f631101c291346a2bf20b991
Philipp Schlunder <philipp@schlunder.de>
2020-10-15 18:35:14 +0200

    Adding the app.

---
+++ app.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+import numpy as np
+import pytz
+import streamlit as st
+
+from tools import *
+
+
+# --- Streamlit Seite zusammenstellen ---
+st.title('COVID19 lokale Datenhistorie')
+# Region auswählen:
+# Mögliche Landkreise abrufen, um sie als Auswahl bereitzustellen
+landkreise = landkreise_abrufen()
+if (landkreise is None):
+    st.error("Es konnte keine Verbindung zum Daten-Serive hergestellt werden.")
+# Dortmund raussuchen, um es als Startwert zu setzen.
+dortmund = np.where(landkreise['attributes.Landkreis']=='SK Dortmund')[0][0]
+dortmund = int(dortmund)
+# Landkreise zur Wahlstellen
+nutzer_region = st.selectbox("Land- bzw. Stadtkreis wählen:", landkreise,
+    index=dortmund)
+
+# Aktuelles Datum abrufen, um an einem neuen Tag, die Daten frisch zu laden und
+# nicht den Cache zu nutzen.
+heute = datetime.now(pytz.timezone('Europe/Berlin')).day
+
+# Regionale Daten abrufen:
+# Wir müssen eine Kopie anlegen, da es ein veränderliches Datenobjekt ist,
+# und dies bei direktem Zugriff den Streamlit Cache beeinträchtigen würde.
+daten = lokale_daten_abrufen(region=nutzer_region, heutiger_tag=heute).copy()
+if (daten is None):
+    st.error("Beim Datenabruf ist ein Fehler aufgetreten.")
+
+# Fallzahlen aus den abgerufenen Meldungen extrahieren
+fallzahlen = fallzahlen_extrahieren(daten)
+
+# Weitere Kennzahlen berechnen:
+# Zunächst wurden die gesammelten Meldungen pro Tag gesammelt. Häufig werden 
+# jedoch Mittelwerte über die letzten 7 Tage berichtet, diese und potentielle
+# weitere Kennzahlen sollen im Folgenden berechnet und visualisiert werden.
+kennzahlen = {}
+# Auswahl der Tage, die zur Mittelwertbildung verwendet werden sollen.
+# Bei 7 Tagen werden bswp. der aktuelle Tag, sowie die 6 vorherigen genutzt.
+periode = st.sidebar.number_input("Anzahl Tage für eine Periode:", min_value=2,
+    max_value=365, value=7)
+
+# Einwohnerzahlen laden und visualisieren, falls gewünscht:
+woechentliche_faelle = 'Fallzahlen pro 100t Einwohner und Woche berechnen?'
+schranken = False
+if st.sidebar.checkbox(woechentliche_faelle, True):
+    try:
+        # Einwohnerzahlen laden, um diese im Anschluss zur Normierung auf
+        # 100 000 Einwohner nutzen zu können.
+        einwohnerzahlen = einwohnerzahlen_laden()
+        nutzer_einwohner = int(einwohnerzahlen.loc[nutzer_region])
+    except:
+        # falls es Probleme gibt (z.B. Landkreis konnte nicht gefunden werden)
+        # wird eine Ersatzzahl gewählt
+        nutzer_einwohner = 100000
+    # Einwohnerzahl in der Seitenleiste anzeigen und ggfs. abrufen.
+    einwohner = st.sidebar.number_input("Einwohner gewählter Stadt:",
+        min_value=1, max_value=10000000, value=nutzer_einwohner)
+    # Anzahl Fälle pro Woche und 100 000 Einwohner berechnen, da dieser Wert
+    # aktuell von Ländern zur Bewertung der Lage genutzt wird.
+    rel_woechentliche_faelle = fallzahlen.rolling(7).sum() / (einwohner / 1e5)
+    kennzahlen["pro Woche & 100t Einwohner"] = rel_woechentliche_faelle
+
+    # Abfrage, ob aktuelle Grenzwerte eingezeichnet werden sollen
+    grenzwert_text = 'Wöchentliche Grenzwerte einzeichnen?'
+    schranken = st.sidebar.checkbox(grenzwert_text, False)
+
+perioden_mittel = fallzahlen.rolling(periode).mean()
+kennzahlen["{} Tage(s) Mittel".format(periode)] = perioden_mittel
+
+# Visualisierung erstellen:
+# Aktualisierungsdatum der Meldungen aus der letzten genutzten Meldung auslesen
+daten_stand = daten["Datenstand"].iloc[-1]
+
+# Visualisierung erstellen lassen
+fig = visualisieren(fallzahlen, nutzer_region, daten_stand, schranken,
+    kennzahlen)
+# und in der Web-App darstellen
+st.pyplot(fig)
+
+# Auflistung der Fallzahlen des letzten Zeitfensters darstellen
+st.header('Fallzahlen der letzten {} Tage:'.format(periode))
+st.dataframe(fallzahlen[-periode:][::-1])
+
+# Quellangaben zu den Daten darstellen
+st.header('Datenquellen:')
+st.write('- Fallzahlen: [Robert Koch-Institut (RKI)](https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0/data), dl-de/by-2-0.')
+st.write('- Einwohnerzahlen: "**Statistisches Bundesamt (Destatis), 2020**" - "*Daten aus dem Gemeindeverzeichnis Kreisfreie Städte und Landkreise nach Fläche, Bevölkerung und Bevölkerungsdichte*", [Kreisfreie Städte und Landkreise am 31.12.2019](https://www.destatis.de/DE/Themen/Laender-Regionen/Regionales/Gemeindeverzeichnis/Administrativ/04-kreise.html)')
+st.write('- Einwohnerzahlen Berliner Bezirke: [Wikpedia](https://de.wikipedia.org/wiki/Berlin#Stadtgliederung) basierend auf Daten des "**Amt für Statistik** Berlin-Brandenburg" aus dem Bericht "*Statistischer Bericht - A I 5 – hj 2 / 19*", abgerufen am 15.10.2020')
---
+++ tools.py
@@ -0,0 +1,196 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import requests as r
+import streamlit as st
+
+LANDKREIS_QUERY = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=Landkreis&returnGeometry=false&returnDistinctValues=true&outSR=4326&f=json"
+DATEN_QUERY = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=Landkreis%20%3D%20'{}'&outFields=*&outSR=4326&f=json"
+
+
+@st.cache
+def landkreise_abrufen():
+    """Liste aller verfügbaren Landkreise abrufen.
+
+    Returns:
+        Pandas DataFrame mit Liste aller Stadt- und Landkreise.
+        Diese beginnen mit einer Abkürzung SK bzw. LK für Stadt-
+        bzw. Landkreis gefolgt vom Namen der Region.
+        Falls es Probleme beim Abruf gab wird None zurückgegeben."""
+    req = r.get(LANDKREIS_QUERY)
+    if (req.status_code == 200):
+        return pd.json_normalize(req.json()['features'])
+    else:
+        return None
+
+
+@st.cache
+def lokale_daten_abrufen(region: str, heutiger_tag: int) -> pd.DataFrame:
+    """Daten zur gewählten Region abrufen.
+    TODO: Liegen mehr als 5000 Meldungen vor, werden mehrere Abfragen durchge-
+    führt, da die Abfragengrenze bei 5000 zu liegen scheint.
+
+    Args: 
+        region: str
+            Land- oder Stadtkreis für den Daten abgerufen werden sollen.
+            Muss eine Region der vorhandenen Landkreise sein.
+        heutiger_tag: int
+            Dieses Attribut erwartet den aktuellen Tag, lediglich, um den Cache
+            ungültig zu machen, damit an einem anderen Tag der Datensatz frisch
+            geladen wird.
+    Returns: pandas.DataFrame
+        Fallzahlen, und ähnlichen Werten, die vom Robert-Koch-Institut bereit
+        gestellt werden. Daten werden immer für den gesamten verfügbaren Melde-
+        zeitraum abegerufen. Falls es Probleme beim Abruf gab wird None zurück-
+        gegeben."""
+    region = region.replace(" ", "%20")
+
+    # TODO zunächst Anzahl der Meldungen abfragen und bei Wert über 5t,
+    # mehrere Anfragen durchführen und mergen.
+    req = r.get(DATEN_QUERY.format(region))
+    if (req.status_code == 200):
+        # JSON parsen und 'attributes.' Präfix entfernen, welches als Artefakt
+        # vom Auslesen des JSON-Rückgabewertes übrig ist.
+        return pd.json_normalize(req.json()['features']).rename(
+            lambda name: name.replace("attributes.", ""), axis=1)
+    else:
+        return None
+
+
+@st.cache
+def fallzahlen_extrahieren(daten: pd.DataFrame) -> pd.core.series.Series:
+    '''Tageweise aggregierte Fallzahlen für jeden Tag des vorhandenen Zeitraums
+    extrahieren.
+
+    Args:
+        daten: pandas.DataFrame
+            API Rückgabewert vom Robert-Koch-Institut mit Einzelmeldungen.
+
+    Returns: pandas.DataFrame
+        Tageweise aggregierte Fallzahlen, mit Einträgen für jeden Tag des vor-
+        handenen Zeitraums. Aufgefüllte Tage (ohne Meldung) werden mit einer
+        Null versehen.'''
+    # Das Meldedatum liegt jeweils als UNIX-Zeitangabe in ms vor. Umwandeln.
+    daten['Meldedatum'] = pd.to_datetime(daten['Meldedatum'], unit='ms')
+    # Datum als Index verwenden
+    daten.set_index("Meldedatum", inplace=True)
+    # Tägliche Fallzahlen über mehrere Meldungen hinweg, tageweise aufsummieren
+    fallzahlen = daten["AnzahlFall"].groupby(daten.index.date).sum()
+    # Für eine bessere Visualisierung, sollen zudem Tage an denen keine Meldung
+    # eintraf mit einer Null aufgefüllt werden.
+    zeitfenster = pd.date_range(start=fallzahlen.index.min(),
+        end=fallzahlen.index.max())
+    
+    return fallzahlen.reindex(zeitfenster).fillna(0.0)
+
+
+@st.cache
+def einwohnerzahlen_laden() -> pd.core.series.Series:
+    ''' Lädt Einwohnerzahlen aus versch. Quellen. Laden und Aufbereiten der
+    Einwohnerzahlen von DESTATIS und Wikipedia (für Berlinerbezirke, da diese
+    nicht im DESTATIS Datensatz enthalten sind).
+
+    Returns: pandas.DataFrame
+        Tabelle mit zwei Spalten: Landkreis und Einwohnerzahl.
+    '''
+    benoetigte_spalten = [0, 1, 2, 5]
+    spalten_namen = ["Schlüssel", "Region", "Kreis", "Einwohnerzahl"]
+    region_umbenennung = ["SK", "LK", "LK", "SK", "LK Stadtverband"]
+
+    # Einwohnerzahlen von DESTATIS laden.
+    df = pd.read_excel('data/04-kreise.xlsx',
+                       sheet_name=1,
+                       skiprows=3,
+                       skipfooter=16,
+                       usecols=benoetigte_spalten,
+                       dtype=object,
+                       names=spalten_namen)
+    # Manuell irrelevante Einträge entfernen (z.B. von Spaltentiteln).
+    df.drop([0,1,2])
+    # Alle Einträge mit zu kurzem (Länge ungleich 5) Schlüssel aussortieren
+    df = df.drop(index=df[df['Schlüssel'].apply(lambda x:len(str(x)))!=5].index)
+    # Regionen erheben und umbenennen
+    # Regionumwandlung:
+    # - Kreisfreie Stadt & Stadtkreis -> SK
+    # - Kreis & Landkreis -> LK
+    # - Regionalverband -> LK Stadtverband
+    regionen = df['Region'].unique()
+    wörterbuch = dict(zip(regionen,region_umbenennung))
+    df['Region'] = df['Region'].replace(wörterbuch)
+    # Einige Städte müssen gesondert benannt werden:
+    df.loc[377]['Kreis'] = 'Saarbrücken'
+    df.loc[98]['Region'] = 'StadtRegion'
+    df.loc[98]['Kreis'] = 'Aachen'
+    df.loc[35]['Region'] = 'Region'
+    df.loc[35]['Kreis'] = 'Hannover'
+    # übrige Städte sind als Wort(e) des Kreises vor einem Komma zu extrahieren
+    # Bspw.: "Flensburg, Stadt" --> "Flensburg"
+    # Kreisbezeichnungen können auch Leerzeichen oder Bindestrich enthalten
+    df['Kreis'] = df['Kreis'].str.extract(r'([\w\s-]+)')
+    # Landkreis passend zur Schreibweise vom RKI als Index erstellen
+    # Bspw.: "SK Flensburg"
+    df['Landkreis'] = df['Region'] + ' ' + df['Kreis']
+    # und als Index für spätere Zusammenführung von Datensätzen bereitstellen
+    df.set_index('Landkreis', inplace=True)
+
+    # Berlin ist bei DESTATIS nicht in Bezirke aufgeteilt, daher anfügen der
+    # Einwohnerzahlen pro Bezirk von Wikipedia:
+    # https://de.wikipedia.org/wiki/Berlin#Stadtgliederung (15.10.2020)
+    berlin = pd.read_csv('data/Berlin.csv',
+                         usecols=['Bezirk', 'Einwohnerzahl'],
+                         dtype=object)
+    berlin.set_index('Bezirk', inplace=True)
+    
+    # Einwohnzerzahlen beider Quellen zusammenführen
+    df = df['Einwohnerzahl'].append(berlin['Einwohnerzahl'])
+
+    return df
+
+def visualisieren(fallzahlen: pd.core.series.Series, bezirk: str, stand: str,
+        schranken: bool=False, kennzahlen: dict=None):
+    '''Fallzahlen und etwaige Kennzahlen visualisieren.
+    
+    Args:
+        fallzahlen: pd.core.series.Series
+            Pandas Serie mit dem Meldedatum als Index und den jeweiligen Fall-
+            zahlen als Wert.
+        bezirk: str
+            Name des Bezirks, wird als Titel des Plots verwendet.
+        stand:  str
+            Abrufdatum gemeldeter Fallzahlen, wird im Titel des Plots verwendet.
+        kennzahlen: dict
+            Dictionary mit Name einer jeweiligen Fallzahl als Key (wird in der
+            Legende des Plots genutzt), und zu visualisierenden Kennzahlen als 
+            Value. Es wird eine Kennzahl pro Tag erwartet.'''
+    # Plottingbereich leeren
+    plt.close('all')
+    # und Seitenverhältnis einstellen
+    plt.figure(figsize=(10, 5))
+    
+    # Fallzahlen als Stufenplot mit Schattierung unter den Stufen darstellen
+    plt.fill_between(fallzahlen.index, fallzahlen.values, step="pre", alpha=0.4)
+    plt.plot(fallzahlen, drawstyle='steps', label="tägl. Fallzahlen")
+    
+    # Kennzahlen hinzufügen, falls vorhanden
+    if (kennzahlen != None):
+        for kennzahl, werte in kennzahlen.items():
+            plt.plot(werte, drawstyle='steps', label=kennzahl)
+
+    if schranken:
+        plt.hlines([50], fallzahlen.index.min(), fallzahlen.index.max(), "red",
+            label="Grenzwert")
+        plt.hlines([35], fallzahlen.index.min(), fallzahlen.index.max(), "orange",
+            label="Warnwert")
+    
+    # Achsen und Plot beschriften, sowie Datumslegende lesbar ausrichten
+    plt.xticks(rotation=45)
+    plt.xlabel('Meldedatum')
+    plt.ylabel('gemeldete Fallzahlen')
+    plt.title(bezirk+", Stand: "+stand)
+    
+    # Legende mittig anzeigen, falls Kennzahlen vorhanden sind
+    if (kennzahlen != None):
+        plt.legend(loc='upper center')
+    # und zur Orientierung ein Raster (im Plot) einfügen
+    plt.grid()
+    return plt
